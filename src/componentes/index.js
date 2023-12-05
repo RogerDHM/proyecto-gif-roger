fbac57dd27859d27fd8649cb93c8050922aba286
RogerDHM <le20080925@merida.tecnm.mx>
2023-12-04 23:29:38 -0600

    first commit

---
+++ src/componentes/index.js
@@ -0,0 +1,5 @@
+/* en este archivo exportamos los componentes para que se puedan llamar en las funciones de los otros archivos, algo asi como herencia */
+
+export * from './AddCategory';
+export * from './GifGrid';
+export * from './GifItem';
